a5b649fbf9417e42ef4a67ea53c85e5d0dc893f4
julie.bonnet <julie.bonnet@iut-rodez.fr>
2023-12-14 11:33:33 +0100

    Ajout méthodes boutons

--- SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/MainActivity.java
+++ SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.saedolistocks5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Méthode appelée automatiquement quand l'utilisateur clique sur le bouton.
+     *
+     * @param bouton initialisé automatiquement avec le bouton à l'origine du clic (ici le
+     * bouton mode connexion).
+     */
+    private void onClickModeDeco (View bouton) {
+        // TODO
+    }
+
+    /**
+     * Méthode appelée automatiquement quand l'utilisateur clique sur le bouton.
+     *
+     * @param bouton initialisé automatiquement avec le bouton à l'origine du clic (ici le
+     * bouton mode connexion).
+     */
+    private void onClickModeCo (View bouton) {
+        // TODO
     }
 }
