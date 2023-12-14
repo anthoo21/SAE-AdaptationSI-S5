35e0bbf899b8d0ccd79848cadaf5d6839c95cd44
jeff7738 <jean-francois.froment@iut-rodez.fr>
2023-12-14 08:21:49 +0100

    code java button accueil

---
+++ SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/ListeActivity.java
@@ -0,0 +1,14 @@
+package com.example.saedolistocks5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.liste_activity);
+    }
+}
--- SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/MainActivity.java
+++ SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.saedolistocks5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickCo(View Button){
+        Intent intention = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intention);
+    }
+
+    public void clickDeco(View Button){
+        Intent intention = new Intent(MainActivity.this, ListeActivity.class);
+        startActivity(intention);
     }
 }
