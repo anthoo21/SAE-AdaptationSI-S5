db1787ca6ca6e637c33bbb193c6a15b88db6a473
julie.bonnet <julie.bonnet@iut-rodez.fr>
2023-12-15 16:59:42 +0100

    Dev retour en cours

--- SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/LoginActivity.java
+++ SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.saedolistocks5;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,37 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+    }
+
+    /**
+     * Méthode invoquée automatiquement lors d'un clic sur l'image bouton
+     * de retour vers l'activité principale
+     * @param view  source du clic
+     */
+    public void onClickRetour(View view) {
+
+        // création d'une intention pour informer l'activté parente
+        Intent intentionRetour = new Intent();
+
+        // retour à l'activité parente et destruction de l'activité fille
+        setResult(Activity.RESULT_OK, intentionRetour);
+        finish(); // destruction de l'activité courante
+    }
+
+    @Override
+    /**
+     * Méthode invoquée automatiquement lors d'un clic sur le bouton
+     * de retour du téléphone
+     * @param view  source du clic
+     * @deprecated
+     */
+    public void onBackPressed() {
+
+        // création d'une intention pour informer l'activté parente
+        Intent intentionRetour = new Intent();
+
+        // retour à l'activité parente et destruction de l'activité fille
+        setResult(Activity.RESULT_OK, intentionRetour);
+        finish(); // destruction de l'activité courante
     }
 }
