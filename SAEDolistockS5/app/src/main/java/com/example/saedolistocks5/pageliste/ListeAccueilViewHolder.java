856ae385dcc8f78fa6133f73d73d7c701c116ff9
julie.bonnet <julie.bonnet@iut-rodez.fr>
2024-01-30 17:36:41 +0100

    Développement de l'affichage des listes de la vue #22

---
+++ SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/pageliste/ListeAccueilViewHolder.java
@@ -0,0 +1,56 @@
+package com.example.saedolistocks5.pageliste;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.saedolistocks5.R;
+
+/**
+ * Description du contenant des items de la liste de type RecyclerView
+ * permettant d'afficher des photos de Paris.
+ * @author GP1
+ * @version 1.0
+ */
+public class ListeAccueilViewHolder extends RecyclerView.ViewHolder{
+    /**
+     * TextView qui contient le libellé de la liste
+     */
+    private TextView titreListe;
+
+    /**
+     * TextView qui contient la date de créa de la liste
+     */
+    private TextView dateCrea;
+
+    /**
+     * TextView qui contient l'heure de créa de la liste
+     */
+    private TextView heureCrea;
+
+    /**
+     * Constructeur avec en argument une vue correspondant
+     * à un item de la liste
+     * Le constructeur permet d'initialiser les identifiants des
+     * widgets déclarés en tant qu'attributs
+     * @param itemView vue décrivant l'affichage d'un item de la liste
+     */
+    public ListeAccueilViewHolder (View itemView) {
+        super(itemView);
+        titreListe = (TextView) itemView.findViewById(R.id.titreListe);
+        dateCrea = (TextView) itemView.findViewById(R.id.dateCrea);
+        heureCrea = (TextView) itemView.findViewById(R.id.heureCrea);
+    }
+    /**
+     * Permet de placer les informations contenues dans l'argument
+     * dans les widgets d'un item de la liste
+     * @param maListe l'instance qui doit être affichée
+     */
+    public void bind(ListeAccueil maListe){
+        titreListe.setText(maListe.getTitreListe());
+        dateCrea.setText(maListe.getDateCrea());
+        heureCrea.setText(maListe.getHeureCrea());
+    }
+}
--- SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/pageliste/ListeActivity.java
+++ SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/pageliste/ListeActivity.java
@@ -1,16 +1,108 @@
 package com.example.saedolistocks5.pageliste;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.saedolistocks5.R;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
 public class ListeActivity extends AppCompatActivity {
 
+    /**
+     * Liste source des données à afficher :
+     * chaque élément contient une instance de PhotoParis (une photo
+     * et son libellé)
+     */
+    private ArrayList<ListeAccueil> listeAccueil;
+    /**
+     * Element permettant d'afficher la liste des photos
+     */
+    private RecyclerView listeAccueilRecyclerView;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.liste_activity);
+
+        listeAccueilRecyclerView = findViewById(R.id.liste_listes_accueil);
+        initialiseListeAccueil();
+
+        /*
+         * On crée un gestionnaire de layout linéaire, et on l'associe à la
+         * liste de type RecyclerView
+         */
+        LinearLayoutManager gestionnaireLineaire = new LinearLayoutManager(this);
+        listeAccueilRecyclerView.setLayoutManager(gestionnaireLineaire);
+
+        /*
+         * On crée un adaptateur personnalisé et permettant de gérer spécifiquement
+         * l'affichage des instances de type listeAccueil en tant que item de la liste.
+         * Cet adapatateur est associé au RecyclerView
+         */
+        ListeAccueilAdapter adaptateur = new ListeAccueilAdapter(listeAccueil);
+        listeAccueilRecyclerView.setAdapter(adaptateur);
+    }
+    /**
+     * Méthode pour initialiser la liste des photos et des textes
+     */
+    private void initialiseListeAccueil() {
+        listeAccueil = new ArrayList<>();
+        listeAccueil.add(new ListeAccueil("Liste N°1", "Date de création : 01/01/01", "9:30"));
+        listeAccueil.add(new ListeAccueil("Liste N°2", "Date de création : 12/02/01", "10:30"));
+        listeAccueil.add(new ListeAccueil("Liste N°3", "Date de création : 16/05/01", "22:10"));
+    }
+
+    /* ===============================================================================
+     *                       méthodes pour gérer le menu CONTEXTUEL
+     * ===============================================================================
+     */
+
+    /**
+     * Méthode invoquée automatiquement lorsque l'utiisateur active le menu contextuel
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        /*
+         *  on désérialise le fichier XML décriant le menu et on l'associe
+         *  au menu argument (celui qui a été activé)
+         */
+        new MenuInflater(this).inflate(R.menu.menu_liste_accueil, menu);
+    }
+
+    /**
+     * Méthode invoquée automatiquement lorsque l'utilisateur choisit une option
+     * dans un menu contextuel
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        /*
+         *  on accède à des informations supplémentaires sur la vue associée
+         *  au menu activé. L'information qui nous intéresse est la position
+         *  de l'élément de la liste sur lequel l'utilisateur a cliqué pour
+         *  activer le menu.
+         */
+        AdapterView.AdapterContextMenuInfo information =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        // selon l'option sélectionnée dans le menu, on réalise le traitement adéquat
+        if (item.getItemId() == R.id.optionVisualisation) { // visualisation d'une liste
+
+        } else { // modification d'une liste
+
+        }
+        return (super.onContextItemSelected(item));
     }
 }
