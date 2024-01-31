a3559cf912e8081df8f74fba18d97e2eb49c8056
jeff7738 <jean-francois.froment@iut-rodez.fr>
2024-01-31 16:16:03 +0100

    Ajout pour futur lecture de fichier

--- SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/Visualisation.java
+++ SAEDolistockS5/app/src/main/java/com/example/saedolistocks5/Visualisation.java
@@ -1,6 +1,7 @@
 package com.example.saedolistocks5;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +11,18 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Visualisation extends AppCompatActivity {
 
     private ArrayList<ItemVisualisation> listeArticles;
     private RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +37,52 @@ public class Visualisation extends AppCompatActivity {
 
         ItemVisualisationAdpater adpater = new ItemVisualisationAdpater(listeArticles);
         recyclerView.setAdapter(adpater);
+
     }
 
     private void intialiserItem() {
         listeArticles = new ArrayList<>();
-        // bouchon
+        String infoLu,
+                libel,
+                qty;
+        try {
+            // TODO Faire autre méthode pour récup info fichier via intention ?
+            FileOutputStream fichierInser = openFileOutput("Visu.txt",Context.MODE_PRIVATE);
+            fichierInser.write("Ajout".getBytes());
+            fichierInser.write("\n".getBytes());
+            fichierInser.write("Je suis une liste".getBytes());
+            fichierInser.write("\n".getBytes());
+            fichierInser.write("250".getBytes());
+            fichierInser.write("\n".getBytes());
+            fichierInser.write("Modification".getBytes());
+            fichierInser.write("\n".getBytes());
+            fichierInser.write("Je suis un truc".getBytes());
+            fichierInser.write("\n".getBytes());
+            fichierInser.write("120".getBytes());
+            fichierInser.write("\n".getBytes());
+            fichierInser.close();
+
+            InputStreamReader fichier = new InputStreamReader(openFileInput("Visu.txt"));
+            BufferedReader fichiertxt = new BufferedReader(fichier);
+            // TODO for i nombre d'articles à recup de la liste via intention ?
+            for (int i = 0; i <= 1; i++) {
+                infoLu = fichiertxt.readLine();
+                libel = fichiertxt.readLine();
+                qty = fichiertxt.readLine();
+                fichiertxt.mark(0);
+                fichiertxt.reset();
+                listeArticles.add(new ItemVisualisation(infoLu,libel,qty));
+            }
+        } catch (FileNotFoundException e) {
+           // throw new RuntimeException(e); peu importe
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        /* bouchon
         listeArticles.add(new ItemVisualisation("Ajout","Peluche 33 pouces en forme d'ours polaire","50"));
         listeArticles.add(new ItemVisualisation("test2","test2","25000"));
+         */
     }
 
     /**
